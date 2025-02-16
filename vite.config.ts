f33e3bc0e9494dcdcd0271a8c518c59b32a9b058
Pedro <pedrocs378@gmail.com>
2025-02-15 21:46:09 -0300

    chore: add manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('pages/')) return 'pages'
+          if (id.includes('components/')) return 'components'
+        }
+      }
+    }
+  }
 })
